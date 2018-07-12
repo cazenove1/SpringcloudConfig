e02d8e771b6964d22aa45d2d3039e581754bf4d8
cazen <Cazen_java@163.cm>
2018-07-12 19:18:08 +0800

    修改仓库配置

---
+++ src/main/java/com/caiduoduo/SpringcloudconfigApplication.java
@@ -0,0 +1,12 @@
+package com.caiduoduo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudconfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudconfigApplication.class, args);
+	}
+}
